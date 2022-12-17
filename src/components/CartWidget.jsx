31454fad79d62fd7cbe7d1df4733dded9d2c6cca
Javier Veron <javier.veron@gmail.com>
2022-12-16 21:25:03 -0300

    Correccion del Cart Widget

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -2,15 +2,19 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { CartContext } from "./context/CartContext";
 
-const CartWidget = () => {
-    const {cartTotal} = useContext(CartContext);
-
+const BotonCarrito = ({total}) => {
     return (
         <Link to={"/cart"} className="btn btn-warning position-relative">
             <img src="/images/cart.svg" alt="Carrito" width="32" />
-            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{cartTotal()}</span>
+            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{total}</span>
         </Link>
     )
+}
+
+const CartWidget = () => {
+    const {cartTotal} = useContext(CartContext);
+
+    return cartTotal() > 0 ? <BotonCarrito total={cartTotal()} />  : "";
 }
 
 export default CartWidget;
